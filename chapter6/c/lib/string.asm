4bba47beb4c6a550a975574b434e6e5a952ac80a
chinbor <949524548@qq.com>
2019-05-17 17:56:56 +0800

    the exp of chapter6/c

---
+++ chapter6/c/lib/string.asm
@@ -0,0 +1,85 @@
+
+; ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+;                              string.asm
+; ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+;                                                       Forrest Yu, 2005
+; ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+[SECTION .text]
+
+; 导出函数
+global	memcpy
+global	memset
+
+; ------------------------------------------------------------------------
+; void* memcpy(void* es:pDest, void* ds:pSrc, int iSize);
+; ------------------------------------------------------------------------
+memcpy:
+	push	ebp
+	mov	ebp, esp
+
+	push	esi
+	push	edi
+	push	ecx
+
+	mov	edi, [ebp + 8]	; Destination(地址为0x00031820)
+	mov	esi, [ebp + 12]	; Source（地址为0x0009013d）
+	mov	ecx, [ebp + 16]	; Counter(值为0x00000020)
+.1:
+	cmp	ecx, 0		; 判断计数器
+	jz	.2		; 计数器为零时跳出
+
+	mov	al, [ds:esi]		; ┓
+	inc	esi					; ┃
+							; ┣ 逐字节移动
+	mov	byte [es:edi], al	; ┃
+	inc	edi					; ┛
+
+	dec	ecx		; 计数器减一
+	jmp	.1		; 循环
+.2:
+	mov	eax, [ebp + 8]	; 返回值
+
+	pop	ecx
+	pop	edi
+	pop	esi
+	mov	esp, ebp
+	pop	ebp
+
+	ret			; 函数结束，返回
+; memcpy 结束-------------------------------------------------------------
+
+
+; ------------------------------------------------------------------------
+; void memset(void* p_dst, char ch, int size);
+; ------------------------------------------------------------------------
+memset:
+	push	ebp
+	mov	ebp, esp
+
+	push	esi
+	push	edi
+	push	ecx
+
+	mov	edi, [ebp + 8]	; Destination
+	mov	edx, [ebp + 12]	; Char to be putted
+	mov	ecx, [ebp + 16]	; Counter
+.1:
+	cmp	ecx, 0		; 判断计数器
+	jz	.2			; 计数器为零时跳出
+
+	mov	byte [edi], dl		; ┓
+	inc	edi					; ┛
+
+	dec	ecx		; 计数器减一
+	jmp	.1		; 循环
+.2:
+
+	pop	ecx
+	pop	edi
+	pop	esi
+	mov	esp, ebp
+	pop	ebp
+
+	ret			; 函数结束，返回
+; ------------------------------------------------------------------------
