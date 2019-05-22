4935132d83424db757981a25842d2a073f8ad889
chinbor <949524548@qq.com>
2019-05-22 21:09:34 +0800

    the exp of chapter6/m & n

---
+++ chapter6/m/include/string.h
@@ -0,0 +1,10 @@
+/* memcpy函数的声明 
+ * string.h
+ * chinbor
+ * 19.5.13
+*/
+
+// string.asm中定义
+PUBLIC	void*	memcpy(void* p_dst, void* p_src, int size);
+PUBLIC	void	memset(void* p_dst, char ch, int size);
+PUBLIC	char*	strcpy(char* p_dst, char* p_src);
---
+++ chapter6/n/include/const.h
@@ -0,0 +1,65 @@
+/* 一些常量的定义
+ * const.h
+ * chinbor
+ * 19.5.13
+*/
+
+#ifndef	_ORANGES_CONST_H_
+#define	_ORANGES_CONST_H_
+
+/* EXTERN is defined as extern except in global.c */
+#define EXTERN extern
+
+/* 函数类型 */
+#define	PUBLIC		/* PUBLIC is the opposite of PRIVATE,什么都没有相当于公有 */
+#define	PRIVATE	static	/* PRIVATE x limits the scope of x ，只能文件自己用 */
+
+/* Boolean */
+#define	TRUE	1
+#define	FALSE	0
+
+/* GDT 和 IDT 中描述符的个数,由于gdtr中的表界限为16位，所以最大为ffffh，所以长度为65539字节，但是一个描述符8字节，所以最多8192个描述符，我们这里只定义了128个描述符 */
+#define	GDT_SIZE	128
+/* 这里固定idt有256个描述符 */
+#define IDT_SIZE    256
+
+/* 权限 */
+#define PRIVILEGE_KRNL	0
+#define PRIVILEGE_TASK	1
+#define PRIVILEGE_USER	3
+
+/* RPL */
+#define	RPL_KRNL	SA_RPL0      
+#define	RPL_TASK	SA_RPL1		
+#define	RPL_USER	SA_RPL3		
+
+/* 8259a interrupt controller ports */
+#define INT_M_CTL		0x20     
+#define INT_M_CTLMASK	0x21
+#define INT_S_CTL		0xA0
+#define INT_S_CTLMASK	0xA1
+
+/* 8253/8254 PIT (Programmable Interval Timer) */
+#define TIMER0         0x40		/* I/O port for timer channel 0 */
+#define TIMER_MODE     0x43		/* I/O port for timer mode control */
+#define RATE_GENERATOR 0x34		/* 00-11-010-0 :* Counter0 - LSB then MSB - rate generator - binary*/
+#define TIMER_FREQ     1193182L /* clock frequency for timer in PC and AT，这里再整数后面加L表示浮点数 */
+#define HZ             100		/* clock freq (software settable on IBM-PC) */
+
+/* 硬件中断 */
+#define	NR_IRQ			16	/* Number of IRQs */
+#define	CLOCK_IRQ		0	/* 时钟中断号 */
+#define	KEYBOARD_IRQ	1	/* 键盘中断号 */
+#define	CASCADE_IRQ		2	/* cascade enable for 2nd AT controller */
+#define	ETHER_IRQ		3	/* default ethernet interrupt vector */
+#define	SECONDARY_IRQ	3	/* RS232 interrupt vector for port 2 */
+#define	RS232_IRQ		4	/* RS232 interrupt vector for port 1 */
+#define	XT_WINI_IRQ		5	/* xt winchester */
+#define	FLOPPY_IRQ		6	/* floppy disk */
+#define	PRINTER_IRQ		7
+#define	AT_WINI_IRQ		14	/* at winchester */
+
+/* system call */
+#define NR_SYS_CALL		1
+
+#endif /* _ORANGES_CONST_H_ */
